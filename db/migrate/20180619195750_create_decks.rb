c54970292f8e5d9658e49d74e1d84b587c84d2cd
Rafael Moraes <roliveira.moraes@gmail.com>
2018-06-19 20:32:37 -0300

    changed card migration

--- db/migrate/20180619195750_create_decks.rb
+++ db/migrate/20180619195750_create_decks.rb
@@ -2,9 +2,9 @@ class CreateDecks < ActiveRecord::Migration[5.2]
   def change
     create_table :decks do |t|
       t.references :user, foreign_key: true
-      t.string :name, limit: 75
+      t.string :name, limit: 75, null: false
       t.string :detail, limit: 255
-      t.integer :cards_count
+      t.integer :cards_count, null: false, default: 0
 
       t.timestamps
     end
