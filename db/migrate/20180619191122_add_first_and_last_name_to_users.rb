7e702a77aea660df1ca7f07744f3ee3fae0c46d4
Rafael Moraes <roliveira.moraes@gmail.com>
2018-06-26 16:46:31 -0300

    fix rubocop

--- db/migrate/20180619191122_add_first_and_last_name_to_users.rb
+++ db/migrate/20180619191122_add_first_and_last_name_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#Migration to add extra attributes to model User
+# Migration to add extra attributes to model User
 class AddFirstAndLastNameToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string, null: false, limit: 40
