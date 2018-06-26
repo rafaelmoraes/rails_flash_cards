eb87b03cfccb05877818e24ddb9cbde9f3ed1e5a
Rafael Moraes <roliveira.moraes@gmail.com>
2018-06-26 15:34:49 -0300

    fixed rubocop suggestions

--- test/controllers/root_route_test.rb
+++ test/controllers/root_route_test.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# Tests if root path the sign in page
 class RootRouteTest < ActionDispatch::IntegrationTest
   test 'should open the sign_in page' do
     assert_routing '/', controller: 'devise/sessions', action: 'new'
