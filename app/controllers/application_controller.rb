7b0ae14cc8bf1e58781043b2f9a4bbb4b4ff1fca
Rafael Moraes <roliveira.moraes@gmail.com>
2018-06-29 17:19:28 -0300

    implemented I18n Url strategy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,18 @@
 
 # ApplicationController
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) ||
+                  params[:locale] ||
+                  I18n.default_locale
+  end
 
   protected
 
