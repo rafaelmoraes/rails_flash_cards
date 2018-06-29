2f5b3e90c92e7aa12093409e096fb07ea4bfa95d
Rafael Moraes <roliveira.moraes@gmail.com>
2018-06-29 17:19:28 -0300

    implemented I18n Url strategy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'decks#index'
+  scope "(:locale)", locale: /en|pt/ do
+    devise_for :users
+    root to: 'decks#index'
 
-  resources :decks do
-    resources :cards
+    resources :decks do
+      resources :cards
+    end
   end
 end
