2d59e96ccfde5527bef66dd0ef387a06994191b5
Rafael Moraes <roliveira.moraes@gmail.com>
2018-07-02 19:42:22 -0300

    droped locale in URL by locale as query parameter

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Necessary to use locale in URL
-  scope '(:locale)', locale: /en|pt-BR/ do
-    devise_for :users
-    root to: 'decks#index'
+  devise_for :users
+  root to: 'decks#index'
 
-    resources :decks do
-      resources :cards
-    end
+  resources :decks do
+    resources :cards
   end
 end
