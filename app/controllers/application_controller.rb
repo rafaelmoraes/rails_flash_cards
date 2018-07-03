0830134052434377b4f178aec3dc18f47df642dd
Rafael Moraes <roliveira.moraes@gmail.com>
2018-07-03 14:16:15 -0300

    translated Deck and return to strategy of locale by URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def self.default_url_options
+    { locale: I18n.locale }
+  end
+
   protected
 
   def set_locale
     I18n.locale = current_user.try(:locale) ||
                   params[:locale] ||
                   I18n.default_locale
-  end
-
-  def self.default_url_options
-    { locale: I18n.locale }
   end
 
   def configure_permitted_parameters
