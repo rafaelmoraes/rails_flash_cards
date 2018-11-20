b2732b621c42b24367a743c6a877cca4a42a9aec
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-20 17:40:29 -0200

    written system tests helpers for help to use I18n

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :firefox, screen_size: [1280, 720]
+
+  protected
+    def t(key, *params)
+      return I18n.t(key, *params) if key.is_a? Symbol
+      I18n.t("#{i18n_plural_scope}.#{key}", *params)
+    end
+
+    def t_fill_in(attr_name, *params)
+      fill_in I18n.t("activerecord.attributes.#{i18n_singular_scope}.#{attr_name}"),
+              *params
+    end
+
+    def t_click_submit(key, *params)
+      click_on I18n.t("helpers.submit.#{key}",
+                      model: I18n.t("activerecord.models.#{i18n_singular_scope}")),
+               *params
+    end
+
+    def t_assert_text(key, *params)
+      assert_text t(key), *params
+    end
+
+    def i18n_plural_scope
+      @_i18n_plural_scope ||= self.class.to_s.split("Test").first.downcase
+    end
+
+    def i18n_singular_scope
+      @_i18n_singular_scope ||= i18n_plural_scope.singularize
+    end
 end
--- test/system/decks_test.rb
+++ test/system/decks_test.rb
@@ -5,7 +5,6 @@ require "application_system_test_case"
 class DecksTest < ApplicationSystemTestCase
   setup do
     @deck = decks(:always_valid)
-    I18n.locale = @deck.user.setting.locale
   end
 
   test "visiting the index" do
@@ -13,10 +12,23 @@ class DecksTest < ApplicationSystemTestCase
     assert_selector "h1", text: t("index.title")
   end
 
-  test "go to new Deck" do
+  test "go to new Deck from index" do
     visit decks_url
     click_on t("index.new")
     assert_selector "h1", text: t("new.title")
+  end
+
+  test "start review from index decks list" do
+    visit decks_url
+    click_on t("index.start_review"), match: :first
+    assert_selector "h1", text: @deck.name
+  end
+
+  test "go to new card from index decks list" do
+    visit decks_url
+    click_on t("index.new_card"), match: :first
+    assert_selector "h1", text: @deck.name
+    assert_selector "h2", text: t(:"cards.new.title")
   end
 
   test "creating a Deck" do
@@ -26,7 +38,7 @@ class DecksTest < ApplicationSystemTestCase
 
     t_fill_in :detail, with: "Super Deck to learn super things"
     t_fill_in :name, with: "Super Deck"
-    t_click_on :create
+    t_click_submit :create
 
     t_assert_text "create.created"
   end
@@ -39,40 +51,65 @@ class DecksTest < ApplicationSystemTestCase
 
   test "updating a Deck" do
     visit deck_url(@deck)
-    click_on t(:edit), match: :first
+    click_on t(:edit)
 
     t_fill_in :detail, with: "#{@deck.detail} EDITED"
     t_fill_in :name, with: "#{@deck.name} EDITED"
-    t_click_on :update
+    all("div.choose-color label").sample.click
+    t_click_submit :update
 
     t_assert_text "update.updated"
   end
-  #
-  # test "destroying a Deck" do
-  #   visit l_decks_url
-  #   page.accept_confirm do
-  #     click_on "Destroy", match: :first
-  #   end
-  #
-  #   assert_text "Deck was successfully destroyed"
-  # end
 
-  private
-    def t(key)
-      return I18n.t(key) if key.is_a? Symbol
-      I18n.t("decks.#{key}")
+  test "should display the update Deck error messages" do
+    visit deck_url(@deck)
+    click_on t(:edit)
+
+    t_fill_in :detail, with: "#{@deck.detail * 50 }"
+    t_fill_in :name, with: ""
+    t_click_submit :update
+
+    assert_selector "#error_explanation"
+  end
+
+  test "destroying a Deck" do
+    visit deck_url(@deck)
+    page.accept_confirm do
+      click_on t(:destroy)
     end
 
-    def t_fill_in(attr_name, *params)
-      fill_in I18n.t("activerecord.attributes.deck.#{attr_name}"), *params
-    end
+    t_assert_text "destroy.destroyed"
+  end
 
-    def t_click_on(key)
-      click_on I18n.t("helpers.submit.#{key}",
-                      model: I18n.t("activerecord.models.deck"))
-    end
+  test "go to add new card" do
+    visit deck_url(@deck)
+    click_on t("show.new_card")
+    assert_selector "h1", text: @deck.name
+    assert_selector "h2", text: t(:"cards.new.title")
+  end
 
-    def t_assert_text(key)
-      assert_text t(key)
-    end
+  test "go to edit Deck" do
+    visit deck_url(@deck)
+    click_on t(:edit)
+    assert_selector "h1", text: t("edit.title")
+  end
+
+  test "go to search cards" do
+    visit deck_url(@deck)
+    click_on t("show.search_cards")
+    assert_selector "h1", text: @deck.name
+    assert_selector "button", text: t(:"cards.index.search")
+  end
+
+  test "go to review" do
+    visit deck_url(@deck)
+    click_on t("show.start_review")
+    assert_selector "h1", text: @deck.name
+    assert_selector "button", text: t(:"review_sessions.show.answer")
+  end
+
+  test "should hidden review session start button" do
+    visit deck_url(decks(:no_cards))
+    assert_no_selector "button", text: t("show.start_review")
+  end
 end
