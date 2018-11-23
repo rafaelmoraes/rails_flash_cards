6d44e0447f98a7a8e4c79deaa2e5268c509a52da
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-23 16:04:46 -0200

    written Admin::User System Test

--- test/system/admin/users_test.rb
+++ test/system/admin/users_test.rb
@@ -1,41 +1,26 @@
+# frozen_string_literal: true
+
 require "application_system_test_case"
 
 class Admin::UsersTest < ApplicationSystemTestCase
   setup do
-    @user = admin_users(:one)
+    @admin = users(:always_valid)
   end
 
   test "visiting the index" do
     visit admin_users_url
-    assert_selector "h1", text: "Admin/Users"
-  end
-
-  test "creating a User" do
-    visit admin_users_url
-    click_on "New Admin/User"
-
-    click_on "Create User"
-
-    assert_text "User was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a User" do
-    visit admin_users_url
-    click_on "Edit", match: :first
-
-    click_on "Update User"
-
-    assert_text "User was successfully updated"
-    click_on "Back"
+    assert_selector "h1", text: t("index.title")
+    total_users = User.where(admin: false).count
+    assert_equal total_users, all("a", text: t(:destroy)).size
   end
 
   test "destroying a User" do
     visit admin_users_url
+    old_users_count = all("a", text: t(:destroy)).size
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on t(:destroy), match: :first
     end
-
-    assert_text "User was successfully destroyed"
+    t_assert_text "destroy.destroyed"
+    assert_equal (old_users_count - 1), all("a", text: t(:destroy)).size
   end
 end
