af6e7efc9a3e9eb52a1fe967dec9b978952c8193
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-27 10:39:29 -0200

    improvements on how routes deal with locale, now new locales are include on scope locale automatically

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|pt-BR/ do
+  scope "(:locale)", locale: Regexp.new(I18n.available_locales.join("|")) do
     devise_for :users, controllers: { registrations: "registrations" }
     root to: "decks#index"
 
