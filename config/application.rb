88e8aa86aead9a5e9fa2ca22a8d81ca0b94d92ad
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-28 20:32:04 -0200

    disabled ActiveStorage

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,27 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+# Disabled to don't load the ActiveStorage
+# require 'rails/all'
+
+require "rails"
+
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
