87fffe6134aa75ea4e6dd8425a93dedabcf3cd3c
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-30 15:21:19 -0200

    created devise mailer preview

---
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class DeviseMailerPreview < ActionMailer::Preview
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, 123)
+  end
+end
