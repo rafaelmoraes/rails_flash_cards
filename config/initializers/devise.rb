fd8a2e3ed6a907c8e63896c8a0dde997a1e0a1e5
Rafael Moraes <roliveira.moraes@gmail.com>
2018-11-30 15:20:55 -0200

    configured devise mailer from

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = 'bf0bfc3be06d98030c188ac4af5cbd88a3e3d562e6d5ab7c79d8019c35b3a83e9d864353a567cb2f1b1c7f5dd7115bcd6b6a607904c6b6fa14433f786c9cf868'
+  config.secret_key = "bf0bfc3be06d98030c188ac4af5cbd88a3e3d562e6d5ab7c79d8019c35b3a83e9d864353a567cb2f1b1c7f5dd7115bcd6b6a607904c6b6fa14433f786c9cf868"
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = "flashcards@rafaelmoraes.pro"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -29,7 +29,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+  require "devise/orm/active_record"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
